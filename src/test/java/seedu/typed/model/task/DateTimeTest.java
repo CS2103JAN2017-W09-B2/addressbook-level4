1039c0c4c4446c2585092bead14f614a0c43f18b
YIM CHIA HUI <E0003264@u.nus.edu>
2017-03-18 14:27:43 +0800

    Make variables private

--- src/test/java/seedu/typed/model/task/DateTimeTest.java
+++ src/test/java/seedu/typed/model/task/DateTimeTest.java
@@ -8,21 +8,21 @@ import org.junit.Test;
 
 public class DateTimeTest {
 
-    DateTime aprilFoolDay = DateTime.getDateTime(2017, Month.APRIL, 1, 0, 0);
-    DateTime christmasDay = DateTime.getDateTime(2017, Month.DECEMBER, 25, 0, 0);
-    DateTime goodFridayDay = DateTime.getDateTime(2017, Month.APRIL, 18, 0, 0);
+    private DateTime aprilFoolDay = DateTime.getDateTime(2017, Month.APRIL, 1, 0, 0);
+    private DateTime christmasDay = DateTime.getDateTime(2017, Month.DECEMBER, 25, 0, 0);
+    private DateTime goodFridayDay = DateTime.getDateTime(2017, Month.APRIL, 18, 0, 0);
 
-    DateTime dayAfterAprilFoolDay = DateTime.getDateTime(2017, Month.APRIL, 2, 0, 0);
-    DateTime dayAfterChristmasDay = DateTime.getDateTime(2017, Month.DECEMBER, 26, 0, 0);
-    DateTime dayAfterGoodFridayDay = DateTime.getDateTime(2017, Month.APRIL, 19, 0, 0);
+    private DateTime dayAfterAprilFoolDay = DateTime.getDateTime(2017, Month.APRIL, 2, 0, 0);
+    private DateTime dayAfterChristmasDay = DateTime.getDateTime(2017, Month.DECEMBER, 26, 0, 0);
+    private DateTime dayAfterGoodFridayDay = DateTime.getDateTime(2017, Month.APRIL, 19, 0, 0);
 
-    DateTime monthAfterAprilFoolDay = DateTime.getDateTime(2017, Month.MAY, 1, 0, 0);
-    DateTime monthAfterChristmasDay = DateTime.getDateTime(2018, Month.JANUARY, 25, 0, 0);
-    DateTime monthAfterGoodFridayDay = DateTime.getDateTime(2017, Month.MAY, 18, 0, 0);
+    private DateTime monthAfterAprilFoolDay = DateTime.getDateTime(2017, Month.MAY, 1, 0, 0);
+    private DateTime monthAfterChristmasDay = DateTime.getDateTime(2018, Month.JANUARY, 25, 0, 0);
+    private DateTime monthAfterGoodFridayDay = DateTime.getDateTime(2017, Month.MAY, 18, 0, 0);
 
-    DateTime yearAfterAprilFoolDay = DateTime.getDateTime(2018, Month.APRIL, 1, 0, 0);
-    DateTime yearAfterChristmasDay = DateTime.getDateTime(2018, Month.DECEMBER, 25, 0, 0);
-    DateTime yearAfterGoodFridayDay = DateTime.getDateTime(2018, Month.APRIL, 18, 0, 0);
+    private DateTime yearAfterAprilFoolDay = DateTime.getDateTime(2018, Month.APRIL, 1, 0, 0);
+    private DateTime yearAfterChristmasDay = DateTime.getDateTime(2018, Month.DECEMBER, 25, 0, 0);
+    private DateTime yearAfterGoodFridayDay = DateTime.getDateTime(2018, Month.APRIL, 18, 0, 0);
 
     @Test
     public void dateTimeTomorrow_aprilFoolDay_returnTrue() {
