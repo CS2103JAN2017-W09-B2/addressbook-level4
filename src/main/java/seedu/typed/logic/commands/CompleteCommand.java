5776005232e013ae04686b8c7604e71934ea540d
YIM CHIA HUI <E0003264@u.nus.edu>
2017-03-20 16:18:14 +0800

    Added functionality to complete tasks in given range or all of the tasks or just a single task

--- src/main/java/seedu/typed/logic/commands/CompleteCommand.java
+++ src/main/java/seedu/typed/logic/commands/CompleteCommand.java
@@ -1,5 +1,6 @@
 package seedu.typed.logic.commands;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import seedu.typed.commons.core.Messages;
@@ -8,6 +9,7 @@ import seedu.typed.logic.commands.util.CommandTypeUtil;
 import seedu.typed.model.task.ReadOnlyTask;
 import seedu.typed.model.task.Task;
 import seedu.typed.model.task.TaskBuilder;
+import seedu.typed.model.task.UniqueTaskList.DuplicateTaskException;
 
 public class CompleteCommand extends Command {
     public static final String COMMAND_WORD = "finish";
@@ -21,42 +23,73 @@ public class CompleteCommand extends Command {
     public static final String MESSAGE_NOT_COMPLETED = "Task does not exists in the task manager";
     public static final String MESSAGE_ALREADY_COMPLETED = "This task is already completed in the task manager.";
 
-    private final int filteredTaskListIndex;
+    private final int startIndex;
+    private final int endIndex;
 
     /**
-     * @param filteredTaskListIndex
+     * @param startIndex
      *            the index of the task in the filtered task list to complete
      */
-    public CompleteCommand(int filteredTaskListIndex) {
-        assert filteredTaskListIndex > 0;
+    public CompleteCommand(int startIndex) {
+        assert startIndex > 0;
 
         // converts filteredTaskListIndex from one-based to zero-based.
-        this.filteredTaskListIndex = filteredTaskListIndex - 1;
+        this.startIndex = startIndex - 1;
+        this.endIndex = this.startIndex;
+    }
+    /**
+     *
+     * @param startIndex
+     * @param endIndex
+     * Assumes that startIndex <= endIndex
+     */
+    public CompleteCommand(int startIndex, int endIndex) {
+        assert startIndex > 0;
+        assert endIndex > 0;
+
+        this.startIndex = startIndex - 1;
+        this.endIndex = endIndex - 1;
+    }
+    /**
+     * Default constructor assumes complete all tasks in filtered task list
+     */
+    public CompleteCommand() {
+        int size = model.getFilteredTaskList().size();
+        this.startIndex = 0;
+        this.endIndex = size - 1;
+    }
+
+    private List<ReadOnlyTask> getAffectedTasks(int startIndex, int endIndex) throws CommandException {
+        List<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
+        List<ReadOnlyTask> tasksToCompleteList = new ArrayList<>();
+        if (startIndex >= lastShownList.size() || endIndex >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+        }
+
+        for (int i = startIndex; i <= endIndex; i++) {
+            tasksToCompleteList.add(lastShownList.get(i));
+        }
+        return tasksToCompleteList;
+    }
+
+    private void updateCompletedTasks(List<ReadOnlyTask> tasksList) throws DuplicateTaskException {
+        for (int i = 0; i < tasksList.size(); i++) {
+            Task taskToCompleteCopy = new TaskBuilder(tasksList.get(i)).build();
+            Task completedTask = new TaskBuilder(taskToCompleteCopy).isCompleted(true).build();
+            model.updateTask(startIndex + i, completedTask);
+            session.update(CommandTypeUtil.TYPE_EDIT_TASK, taskToCompleteCopy, completedTask);
+        }
     }
 
     @Override
     public CommandResult execute() throws CommandException {
-        List<ReadOnlyTask> lastShownList = model.getFilteredTaskList();
-
-        if (filteredTaskListIndex >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
-        }
-
-        if (filteredTaskListIndex >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
-        }
-
-        ReadOnlyTask taskToComplete = lastShownList.get(filteredTaskListIndex);
-        Task taskToCompleteCopy = new TaskBuilder(taskToComplete).build();
-        Task completedTask = new TaskBuilder(taskToCompleteCopy).isCompleted(true).build();
-
+        List<ReadOnlyTask> tasksToCompleteList = getAffectedTasks(startIndex, endIndex);
         try {
-            model.updateTask(filteredTaskListIndex, completedTask);
-            session.update(CommandTypeUtil.TYPE_EDIT_TASK, (Object) taskToCompleteCopy, (Object) completedTask);
+            updateCompletedTasks(tasksToCompleteList);
         } catch (Exception e) {
             throw new CommandException(MESSAGE_NOT_COMPLETED);
         }
         model.updateFilteredListToShowAll();
-        return new CommandResult(String.format(MESSAGE_COMPLETED_TASK_SUCCESS, taskToComplete));
+        return new CommandResult(String.format(MESSAGE_COMPLETED_TASK_SUCCESS));
     }
 }
