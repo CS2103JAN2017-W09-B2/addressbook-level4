797e9bb7e532cd6e841c4dfc1bc171545b1298cf
YIM CHIA HUI <E0003264@u.nus.edu>
2017-03-21 15:18:39 +0800

    Added Unit testing for ClearCommand

---
+++ src/test/java/commandunittests/ClearCommandTest.java
@@ -0,0 +1,41 @@
+package commandunittests;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.typed.logic.commands.CommandResult;
+
+/**
+ * Unit Testing for ClearCommand
+ * We are testing on whether a populated TaskManager and new TaskManager will be cleared successfully
+ * @author YIM CHIA HUI
+ *
+ */
+public class ClearCommandTest {
+    TestClearCommand dirtyClear;
+    TestClearCommand cleanClear;
+
+    @Before
+    public void setup() {
+        ModelStub dirtyModel = new ModelStub();
+        dirtyModel.addTestTask();
+        ModelStub cleanModel = new ModelStub();
+        dirtyClear = new TestClearCommand();
+        cleanClear = new TestClearCommand();
+        dirtyClear.setModel(dirtyModel);
+        cleanClear.setModel(cleanModel);
+    }
+
+    @Test
+    public void execute_clearDirtyTaskManager_success() {
+        assertEquals(dirtyClear.execute(), new CommandResult("Task manager has been cleared!"));
+    }
+
+    @Test
+    public void execute_clearEmptyTaskManager_success() {
+        assertEquals(cleanClear.execute(), new CommandResult("Task manager has been cleared!"));
+    }
+}
