7f48d30ae3ee90c568591146b14a220b7bcc0f5a
Yong Siang Low <lowyongsiang@gmail.com>
2017-03-30 21:16:37 +0800

    View of pieChart on UI

--- src/main/java/seedu/typed/ui/Chart.java
+++ src/main/java/seedu/typed/ui/Chart.java
@@ -1,8 +1,5 @@
+//@@author A0139392X
 package seedu.typed.ui;
-
-import java.util.ResourceBundle;
-
-import javax.print.DocFlavor.URL;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -10,21 +7,22 @@ import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
+import javafx.scene.shape.Circle;
 
 public class Chart extends UiPart<Region> {
     private static final String FXML = "Chart.fxml";
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
 
     @FXML
     private AnchorPane mainPane;
 
     @FXML
     private PieChart chart;
+
+    @FXML
+    private Circle blockOut;
+
+    @FXML
+    ObservableList<PieChart.Data> pieData;
 
     /**
      * @param placeholder
@@ -39,10 +37,23 @@ public class Chart extends UiPart<Region> {
     void initialize() {
         assert chart != null;
 
-        ObservableList<PieChart.Data> pieData =
-                FXCollections.observableArrayList(new PieChart.Data("Completed", 13), new PieChart.Data("Pending", 10));
+        pieData = FXCollections.observableArrayList(
+                new PieChart.Data("Completed", 10),
+                new PieChart.Data("Pending", 13));
 
         chart.setData(pieData);
         chart.setStartAngle(90);
     }
+
+    @FXML
+void updateValue() {
+//        for (Data d : pieData) {
+//            if (d.getName().equals(name)) {
+//                d.setPieValue(value);
+//                return;
+//            }
+//        }
+ }
+
 }
+//@@author
