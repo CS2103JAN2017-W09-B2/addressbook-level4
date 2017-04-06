2e1e3bac7f13775e519607282804b587761374b8
Le <E0007868@u.nus.edu>
2017-04-07 01:49:35 +0800

    Changed CustomTextField

--- src/main/java/seedu/typed/ui/CustomTextField.java
+++ src/main/java/seedu/typed/ui/CustomTextField.java
@@ -2,27 +2,23 @@ package seedu.typed.ui;
 
 
 import javafx.scene.control.TextField;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 
 public class CustomTextField extends TextField {
 
     @Override
     public void replaceText(int start, int end, String text) {
-        if (text.matches("[a-z]")) {
+       /* if (text.matches("[a-z]")) {
             Text t = new Text("exit");
             t.setFont(Font.font(null, FontWeight.EXTRA_BOLD, 40));
             t.setFill(Color.BLUE);
             String toDisplay = t.getText();
             toDisplay = "<html><font color=\"red\">add</font></html>";
            // this.setStyle("-fx-text-inner-color: red;");
-            this.setText("<pre style='color:#000020;background:#f6f8ff;'>red</pre>");
-            super.replaceText(start, end, toDisplay);
-        } else {
+            this.setText("<pre style='color:#000020;background:#f6f8ff;'>red</pre>");*/
             super.replaceText(start, end, text);
-        }
+       /* } else {
+            super.replaceText(start, end, text);
+        }*/
     }
 
 
