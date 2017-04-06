6676759b0c2e12617e1b63201efe47de65a31241
Yong Siang Low <lowyongsiang@gmail.com>
2017-04-06 09:00:38 +0800

    Remove clear command field to pass travis first

--- src/main/java/seedu/typed/ui/CommandBox.java
+++ src/main/java/seedu/typed/ui/CommandBox.java
@@ -67,7 +67,6 @@ public class CommandBox extends UiPart<Region> {
             setStyleToIndicateCommandFailure();
             logger.info("Invalid command: " + commandTextField.getText());
             resetPointer();
-            commandTextField.clear();
             raise(new NewResultAvailableEvent(e.getMessage()));
         }
     }
