7a121c4f422cc78329392ba24a96d78061b63c4e
YIM CHIA HUI <E0003264@u.nus.edu>
2017-04-10 06:59:16 +0800

    Added to unused folder

---
+++ src/unused/Event.java
@@ -0,0 +1,68 @@
+package seedu.typed.model.task;
+
+import seedu.typed.model.tag.UniqueTagList;
+//@@author A0139379M-unused
+//Team decision to not have a separate class... internally represent all as a Task
+public class Event implements ReadOnlyEvent {
+
+    private Name name;
+    private DateTime startDate;
+    private DateTime endDate;
+    private UniqueTagList tags;
+    private boolean isRecurring;
+    private String details;
+
+    public Event(Name name, DateTime startDate, DateTime endDate, boolean isRecurring,
+            String details, UniqueTagList tags) {
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.tags = tags;
+        this.isRecurring = isRecurring;
+        this.details = details;
+    }
+
+    @Override
+    public Name getName() {
+        return name;
+    }
+
+    @Override
+    public DateTime getStartDate() {
+        return startDate;
+    }
+
+    @Override
+    public DateTime getEndDate() {
+        return endDate;
+    }
+
+    @Override
+    public UniqueTagList getTags() {
+        return tags;
+    }
+
+    @Override
+    public boolean isRecurring() {
+        return isRecurring;
+    }
+
+    @Override
+    public String getDetails() {
+        return details;
+    }
+
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ReadOnlyEvent// instanceof handles nulls
+                        && this.isSameStateAs((ReadOnlyEvent) other));
+    }
+
+    @Override
+    public String toString() {
+        return this.getAsText();
+    }
+
+}
---
+++ src/unused/EventBuilder.java
@@ -0,0 +1,105 @@
+package seedu.typed.model.task;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.typed.commons.exceptions.IllegalValueException;
+import seedu.typed.model.tag.Tag;
+import seedu.typed.model.tag.UniqueTagList;
+import seedu.typed.model.tag.UniqueTagList.DuplicateTagException;
+
+//@@author A0139379M-unused
+//Team decision to not have a separate class... internally represent all as a Task
+/**
+ * EventBuilder faciliates the builder of Event objects
+ * By default, tags and description are empty and isRecurring is false
+ * name, startDate and endDate are required
+ * @param ReadOnlyEvent
+ * @return Event
+ * @author YIM CHIA HUI
+ */
+public class EventBuilder {
+
+    private Name name;
+    private DateTime startDate;
+    private DateTime endDate;
+    private boolean isRecurring;
+    private String details;
+    private UniqueTagList tags;
+
+    public EventBuilder() {
+        this.isRecurring = false;
+        this.details = "";
+        this.tags = new UniqueTagList();
+    }
+
+    public EventBuilder(ReadOnlyEvent event) {
+        this.name = event.getName();
+        this.startDate = event.getStartDate();
+        this.endDate = event.getEndDate();
+        this.isRecurring = event.isRecurring();
+        this.details = event.getDetails();
+        this.tags = event.getTags();
+    }
+
+    public EventBuilder setName(String name) throws IllegalValueException {
+        this.name = new Name(name);
+        return this;
+    }
+
+    public EventBuilder setName(Name name) {
+        this.name = name;
+        return this;
+    }
+
+    public EventBuilder startDate(DateTime startDate) {
+        this.startDate = startDate;
+        return this;
+    }
+
+    public EventBuilder endDate(DateTime endDate) {
+        this.endDate = endDate;
+        return this;
+    }
+
+    public EventBuilder date(DateTime startDate, DateTime endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        return this;
+    }
+
+    public EventBuilder isRecurring(boolean isRecurring) {
+        this.isRecurring = isRecurring;
+        return this;
+    }
+
+    public EventBuilder setDetails(String details) {
+        this.details = details;
+        return this;
+    }
+
+    public EventBuilder addTags(String tag) throws DuplicateTagException, IllegalValueException {
+        this.tags.add(new Tag(tag));
+        return this;
+    }
+
+    public EventBuilder setTags(UniqueTagList tags) {
+        this.tags = tags;
+        return this;
+    }
+
+    public EventBuilder setTags(Set<String> tags) throws IllegalValueException {
+        final Set<Tag> tagSet = new HashSet<>();
+        for (String tagName : tags) {
+            tagSet.add(new Tag(tagName));
+        }
+        this.tags = new UniqueTagList(tagSet);
+        return this;
+    }
+
+    public Event build() {
+        return new Event(this.name, this.startDate, this.endDate, this.isRecurring,
+                this.details, this.tags);
+    }
+
+}
